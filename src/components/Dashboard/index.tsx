eb143a5fb811d179fbce95266f91c79697677ae0
Eryson Miranda <eryson.ktm@gmail.com>
2021-10-08 16:23:42 -0300

    feat: Created transactions table styles

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dahsboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 }
